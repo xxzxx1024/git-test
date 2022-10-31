6eb6b5c8b475f7b39ccf565f88b6f76da740f7a2
gq <putongwow@outlook.com>
2022-10-31 15:08:18 +0800

    first commit by idea

---
+++ src/main/java/com/gq/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.gq.git;
+
+/**
+ * @author zgq
+ * @date 2022/10/31 15:05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git by idea!");
+    }
+}

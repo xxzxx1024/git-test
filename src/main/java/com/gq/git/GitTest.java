7ee256c2be4b75e3ede4c2918d11dcd64d20b510
gq <putongwow@outlook.com>
2022-10-31 15:12:10 +0800

    second commit by idea

--- src/main/java/com/gq/git/GitTest.java
+++ src/main/java/com/gq/git/GitTest.java
@@ -6,6 +6,6 @@ package com.gq.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git by idea!");
+        System.out.println("hello git2 by idea!");
     }
 }

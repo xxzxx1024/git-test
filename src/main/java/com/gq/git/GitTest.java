849770932ac06638223ff3c406118f1a0f36df88
gq <putongwow@outlook.com>
2022-10-31 15:20:48 +0800

    hot-fix commit1 by idea

--- src/main/java/com/gq/git/GitTest.java
+++ src/main/java/com/gq/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git by idea!");
         System.out.println("i am back!");
+        System.out.println("i am hot-fix!");
     }
 }
